d4b2890f67de470185d32c90f8d28080f80f917a
Saraswathi Rajkumar <srajkumar@clouddev.epiqcorp.com>
2018-10-07 22:20:15 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        IWebDriver d = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+        }
+
+        [SetUp]
+        public void Start()
+        {
+            d.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html");
+        }
+
+        [Test]
+        public void Operation()
+        {
+
+            //IWebElement e = d.FindElement(By.Name("q"));
+            //e.SendKeys("hi");
+
+            SeleniumSetMethods.SelectDropDown(d, "TitleId", "Mr.", "Id");
+
+            SeleniumSetMethods.Entertext(d, "Initial", "Execute", "Name");
+
+            SeleniumSetMethods.Click(d, "Save", "Name");
+        }
+        [TearDown]
+        public void Clean()
+        {
+            d.Close();
+        }
+    }
+}
---
+++ ConsoleApp1/SeleniumSetMethods.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumFirst
+{
+    public class SeleniumSetMethods
+    {
+       public static void Entertext(IWebDriver d, string e, string value, string elementtype)
+        {
+            if (elementtype == "id")
+            d.FindElement(By.Id(e)).SendKeys(value);
+            if (elementtype == "Name")
+                d.FindElement(By.Name(e)).SendKeys(value);
+            if (elementtype == "Xpath")
+                d.FindElement(By.XPath(e)).SendKeys(value);
+       }
+        //click into a button, checkbox, option etc.,
+        public static void Click(IWebDriver d, string e,  string elementtype)
+        {
+            if (elementtype == "id")
+                d.FindElement(By.Id(e)).Click();
+            if (elementtype == "Name")
+                d.FindElement(By.Name(e)).Click(); 
+            if (elementtype == "Xpath")
+                d.FindElement(By.XPath(e)).Click(); 
+        }
+
+        //Selecting a drop down control
+        public static void SelectDropDown(IWebDriver d, string e, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+           new SelectElement(d.FindElement(By.Id(e))).SelectByText(value);
+            if (elementtype == "Name")
+                new SelectElement(d.FindElement(By.Name(e))).SelectByText(value);
+            if (elementtype == "Xpath")
+                new SelectElement(d.FindElement(By.XPath(e))).SelectByText(value);
+
+        }
+    }
+}
